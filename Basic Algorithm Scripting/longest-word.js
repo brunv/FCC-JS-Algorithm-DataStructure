def5f64953d95e702c4f601bc0908e6e7cab2708
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-11 19:15:07 -0200

    Update longest-word.js

--- Basic Algorithm Scripting/longest-word.js
+++ Basic Algorithm Scripting/longest-word.js
@@ -2,6 +2,10 @@
  *  Find the longest word in a string
  */
 
+/**
+ *  Basic code solution:
+ */
+
 function findLongestWordLength(str) {
     let words = str.split(' ');
     console.log(words);
@@ -17,3 +21,15 @@ function findLongestWordLength(str) {
 }
   
 console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+
+/**
+ *  Intermediate code solution:
+ */
+
+function findLongestWordLength(str) {
+
+    return str.split(' ').reduce(function(x, y) {
+        return Math.max(x, y.length)
+    }, 0);
+}
