72a25e0285c555b132188b8804ada24f3869a095
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-11 18:05:17 -0200

    Create factorialize-number.js

---
+++ Basic Algorithm Scripting/factorialize-number.js
@@ -0,0 +1,24 @@
+/**
+ *  Return the factorial of the provided integer.
+ * 
+ *  If the integer is represented with the letter n, a factorial is the product
+ *  of all positive integers less than or equal to n.
+ */
+
+function factorialize(num) {
+
+    if (num > 1) {
+        return num * factorialize(num-1);
+    }
+    else if (num == 1) {
+        return num * 1;
+    }
+    else {
+        return 1
+    }
+}
+
+console.log(factorialize(5));   // 120
+console.log(factorialize(1));   // 1
+console.log(factorialize(9));   // 362880
+console.log(factorialize(0));   // 1
