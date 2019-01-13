65cc3e28abc794a995e8693684bea89f9a9e454f
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-13 18:50:27 -0200

    Create basic-object.js

---
+++ Object Oriented Programming/basic-object.js
@@ -0,0 +1,18 @@
+/**
+ *  Create a Basic Object:
+ * 
+ *  Think about things people see everyday, like cars, shops, and birds. These are
+ *  all objects: tangible things people can observe and interact with.
+ *  The qualities, or properties, define what makes up an object. Note that similar
+ *  objects share the same properties, but many have different values for those
+ *  properties.
+ *  Objects in JavaScript are used to model real-world objects, giving them
+ *  properties and behavior just like their real-world counterparts.
+ */
+
+// Here's an example using these concepts to create a duck object:
+
+let duck = {
+    name: "Aflac",
+    numLegs: 2
+};
