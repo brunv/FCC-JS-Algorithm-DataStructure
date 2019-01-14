f5b4f844da2bde6e3f82b829f90d1626f340ae44
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-14 18:38:37 -0200

    Update inheritance.js

--- Object Oriented Programming/inheritance.js
+++ Object Oriented Programming/inheritance.js
@@ -44,3 +44,45 @@ Bird.prototype = {
 Dog.prototype = {
     constructor: Dog
 };
+
+
+/**
+ *  Inherit beavior from a Supertype:
+ * 
+ *  In the previous section, we created a supertype called Animal that defined
+ *  behavior shared by all animals:
+ */
+
+function Animal() { }
+Animal.prototype.eat = function() {
+console.log("nom nom nom");
+};
+
+/**
+ *  This and the next section will cover how to reuse Animal's methods inside Bird
+ *  and Dog without defining them again. It uses a technique called inheritance.
+ *  This section covers the first step: make an instance of the supretype (or
+ *  parent). You already know on way to create as instance of Animal using the
+ *  new operator:
+ */
+
+let animal = new Animal();
+
+/**
+ *  There are some disadvantages when using this syntax for inheritance, which are
+ *  too complex for the scope of this section. Instead, here's an alternative
+ *  approach without those disadvantages:
+ */
+
+let animal = Object.create(Animal.prototype);
+
+/**
+ *  Object.create(obj) creates a new object, and sets obj as the new object's
+ *  prototype. Recall that the prottype is like the "recipe" for creating an
+ *  object. By setting the prototype of animal to be Animal's prototype, you are
+ *  effectively giving the animal instance the same "recipe" as any other instance
+ *  of Animal.
+ */
+
+animal.eat();               // "nom nom nom"
+animal instanceof Animal;   // true
