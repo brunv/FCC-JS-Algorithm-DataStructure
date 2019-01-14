0bd77192b69df218d493f2c7f9220e1b3eee8239
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-14 18:44:39 -0200

    Update inheritance.js

--- Object Oriented Programming/inheritance.js
+++ Object Oriented Programming/inheritance.js
@@ -86,3 +86,20 @@ let animal = Object.create(Animal.prototype);
 
 animal.eat();               // "nom nom nom"
 animal instanceof Animal;   // true
+
+
+/**
+ *  Set the child's prototype to an instance of the parent:
+ * 
+ *  In the previous challenge you saw the first step for inheriting behavior from
+ *  the supertype (or parent) Animal: making a new instace of Animal.
+ *  This section covers the next step: set the prototype of the subtype (or child)
+ *  - in this case, Bird - to be an instance of Animal.
+ *  Remember that the prototype is like the "recipe" for creating an object. In a
+ *  way, the recipe for Bird now includes all the key "ingretiens" of Animal.
+ */
+
+Bird.prototype = Object.create(Animal.prototype);
+
+let duck = new Bird("Donald");
+duck.eat();             // "nom nom nom"
