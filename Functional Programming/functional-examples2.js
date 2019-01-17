3c9d30ce868cc0141250d245406c3095c08b1a21
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-01-16 22:53:54 -0200

    Update functional-examples2.js

--- Functional Programming/functional-examples2.js
+++ Functional Programming/functional-examples2.js
@@ -276,3 +276,29 @@ function nonMutatingSort(arr) {
 
 console.log(nonMutatingSort(globalArray));
 console.log(globalArray);
+
+
+/**
+ *  Split a string into an array using the 'split()' method:
+ * 
+ *  The 'split' method splits a string into an array of strings. It takes an
+ *  argument for the delimiter, which can be a character to use to break up the
+ *  string or a regular expression. For example, if the delimiter is a space, you
+ *  get an array of words, and if the delimiter is an empty string, you get an
+ *  array of each character in the string.
+ * 
+ *  Note: since strings are immutable, the 'split' method makes it easier to work
+ *  with them.
+ */
+
+var str = "Hello World!";
+var bySpace = str.split(" ");   // ["Hello", "World!"]
+
+var otherString = "How9are7you2today";
+var byDigits = otherString.split(/\d/);     // ["How", "are", "you", "today"]
+
+function splitify(str) {
+    return str.split(/\W/);
+}
+
+console.log(splitify("Hello World, I-am code"));
